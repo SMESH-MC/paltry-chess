8c26d0417735e77bbcbc3c013565cba131d944b6
ckoenig86 <admin@kaggnub.de>
2013-08-17 13:26:15 +0200

    board 1. commit

--- chessengine/src/chessengine/Board.java
+++ chessengine/src/chessengine/Board.java
@@ -9,6 +9,16 @@ package chessengine;
  */
 public class Board implements BoardInterface  {
 	
+	//Variablendeklaration
+	
+	public int[] boardArray;
+	
+	//Ende Variablendeklaration
+	
+	public Board() {
+		boardArray = new int[128];
+		
+	}
 	
 
 }
