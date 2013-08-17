95a1c5e14181dcd8e184aa2aa05daa5ab20ce385
Dominik <Dominik@DeepThink>
2013-08-17 16:01:59 +0200

    fenDecoder beta - fertig

--- chessengine/src/chessengine/Board.java
+++ chessengine/src/chessengine/Board.java
@@ -49,13 +49,72 @@ public class Board implements BoardInterface  {
 		return boardArray;
 	}
 	
-	public String FenDecode(String s) {
+	public int[] FenDecode(String s) {
+		int i = 0; //pos im Feld
+		int[] ausgabe = new int[128];
+		Character fen;
 		
-		/*
-		 * if incomingFen = startBoard then initBoard; (mit equals arbeiten)
-		 */
+		if (s.equals(boardStart) ) {
+			InitBoard();
+		}
 		
-		return IncomingFen;
+		while (s != " ") {
+			int pos = 0; //pos in FenString
+			fen = s.charAt(pos);
+			
+			switch (fen) {
+			case 'p':
+				ausgabe[i] = 1;
+				break;
+			case 'P':
+				ausgabe[i] = 11;
+				break;
+			case 'r':
+				ausgabe[i] = 2;
+				break;
+			case 'R':
+				ausgabe[i] = 12;
+				break;
+			case 'n':
+				ausgabe[i] = 3;
+				break;
+			case 'N':
+				ausgabe[i] = 13;
+				break;
+			case 'b':
+				ausgabe[i] = 4;
+				break;
+			case 'B':
+				ausgabe[i] = 14;
+				break;
+			case 'q':
+				ausgabe[i] = 5;
+				break;
+			case 'Q':
+				ausgabe[i] = 15;
+				break;
+			case 'k':
+				ausgabe[i] = 6;
+				break;
+			case 'K':
+				ausgabe[i] = 16;
+				break;
+			case ('1' | '2' | '3' | '4' | '5' | '6' | '7' | '8'):
+				int leerFelder = Integer.parseInt( fen.toString() );
+				for (int j = 0; j < leerFelder; j++) {
+					ausgabe[i] = 0;
+					i++;
+				}
+				break;
+			case '/':
+				i = i + 8;
+				break;
+			}
+			i++;
+			pos++;
+		} //endWhile
+
+		return ausgabe;
 		
 	}
 	
