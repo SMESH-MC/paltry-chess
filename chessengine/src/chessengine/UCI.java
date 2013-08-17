c39a0038e5cae76aeb3fd52fedb3a5562f666fb9
arax89 <alexander-kessler@web.de>
2013-08-17 17:34:33 +0200

    UCI

---
+++ chessengine/src/chessengine/UCI.java
@@ -0,0 +1,21 @@
+/**
+ * The UCI protocol as publiced by Stefan-Meyer Kahlen
+ */
+package chessengine;
+
+/**
+ * @author Alexander Kessler
+ *
+ */
+public class UCI {
+	
+/**
+ * 	GUI to engine :(ueber Reader.readln)
+ * 	uci, debug [on | off], isready, setoption name [value],
+ * 	register, ucinewgame, position[fen | startpos ],
+ * 	go "+ weitere Befehle"(s. http://wbec-ridderkerk.nl/html/UCIProtocol.html),
+ * 	stop, ponderhit, quit 
+ * 
+ */
+
+}
