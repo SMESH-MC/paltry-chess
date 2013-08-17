72a57f0b3bd8700c42a96944a73b18af4740b7ae
Christian <christian.koenig86@web.de>
2013-08-17 14:58:07 +0200

    Methoden

--- chessengine/src/chessengine/Board.java
+++ chessengine/src/chessengine/Board.java
@@ -4,16 +4,21 @@ package chessengine;
 /**
  * @author Christian Koenig & Dominik Erb
  *
- * what a fuck...
  */
 public class Board implements BoardInterface  {
 	
 	//Variablendeklaration
+	private static final String boardStart = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 	
 	public int[] boardArray;
 	
 	//true = weiss  ;  false = schwarz
 	public boolean color;
+	
+	public String boardFen;
+	public String IncomingFen;
+	public String OutgoingFen;
+	
 	
 	
 	//Ende Variablendeklaration
@@ -22,7 +27,46 @@ public class Board implements BoardInterface  {
 		boardArray = new int[128];
 		color = true;
 		
+		
 	}
 	
+	public void InitBoard() {
+		IncomingFen = boardStart;
+		FenDecode(IncomingFen);
+		
+		
+	}
+	
+	public void ResetBoard() {
+		
+	}
 
+	public void BoardOutputFen(String boardFen) {
+		
+	}
+	
+	public int[] getBoard() {
+		return boardArray;
+	}
+	
+	public String FenDecode(String s) {
+		
+		/*
+		 * if incomingFen = startBoard then initBoard; (mit equals arbeiten)
+		 */
+		
+		return IncomingFen;
+		
+	}
+	
+	public String FenEncode() {
+		//decodiert FEN Strings
+		
+		return OutgoingFen;
+		
+	}
+	
+	public void IntToFen() {
+		
+	}
 }
