29a3801c8a1d9d459c4d7d95d7b660fdbcbf1e61
ckoenig86 <christian.koenig86@web.de>
2013-09-04 13:20:43 +0200

    Board Interface (Vorlaeufig)

--- chessengine/src/chessengine/BoardInterface.java
+++ chessengine/src/chessengine/BoardInterface.java
@@ -2,4 +2,18 @@ package chessengine;
 
 public interface BoardInterface {
 
+	public abstract void InitBoard();
+
+	public abstract boolean ResetBoard();
+
+	public abstract void BoardOutputFen(String boardFen);
+
+	public abstract int[] getBoard();
+
+	public abstract int[] FenDecode(String s);
+
+	public abstract String FenEncode();
+
+	public abstract Character IntToFen(int toConvert);
+
 }
