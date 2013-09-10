9aa751b2024c5751e8db019af53ee78a96b2490d
smeshmc <smesh00@yahoo.de>
2013-09-10 15:58:30 +0200

    Zuggenerator in eigenes Package verschoben

---
+++ chessengine/src/chessengine/movegenerator/MoveGeneratorInterface.java
@@ -0,0 +1,12 @@
+package chessengine.movegenerator;
+
+import java.util.LinkedList;
+
+
+/**
+ * @author Schuhmacher, Kaub
+ * @version 201309082306
+ */
+public interface MoveGeneratorInterface {
+	public LinkedList<String> getZuege(String aktuelleFEN);
+}
