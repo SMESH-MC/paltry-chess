aaf969786ada7e61f40e087b70b5217676bf5d03
ckoenig86 <christian.koenig86@web.de>
2013-09-12 14:35:26 +0200

    code entfernt

--- chessengine/src/chessengine/MoveEvaluator.java
+++ chessengine/src/chessengine/MoveEvaluator.java
@@ -13,48 +13,5 @@ public class MoveEvaluator {
 	public String IncomingFEN;
 	public String OutgoingFEN;
 	
-	//Ab hier soll Voodoo folgen (fast fertig)
 	
-	public void init() {
-		
-		zthread = new ZugThread();
-		zthread.start();
-		
-	}
-	
-	public void start() {
-		
-		if (zthread == null) {
-			zthread = new ZugThread();
-			zthread.start();
-		}
-		
-	}
-
-	public void stop() {
-
-		if (zthread != null) {
-			
-			zthread.interrupt();
-			zthread = null;
-			
-		}
-	}
-	
-	public void destroy() {
-		if (zthread != null) {
-			
-			zthread.interrupt();
-			zthread = null;
-			
-		}
-	}
-	
-	class ZugThread extends Thread {
-		//Hier Threadgenerierung
-		public void run(){
-			
-		}
-		
-	}
 }
