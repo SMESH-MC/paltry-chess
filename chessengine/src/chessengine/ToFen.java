29e65ef998e404a18c76bb3f0daecf89a58aa314
ckoenig86 <christian.koenig86@web.de>
2013-10-01 09:46:32 +0200

    -ToFen angelegt
    -Todo geschrieben

---
+++ chessengine/src/chessengine/ToFen.java
@@ -0,0 +1,14 @@
+package chessengine;
+
+public class ToFen extends Board {
+	//Todo
+	//Moves in Liste schreiben
+	//bei jedem neuen element pruefen ob schon vorhanden
+	//je nach startfarbe gerade oder ungerade anzahl in liste ignorieren
+	//aktuelles board aufrufen
+	//kopie des aktuellen board erzeugen
+	//zug darauf ausfuehren
+	//fen aus board generieren
+	//weiterverarbeiten wie urspruenglich geplant
+
+}
