b80b86bf9a0c845d6812902fd31c25865f5ccf62
Thorsten <Thorsten@Thorsten-PC>
2013-10-10 19:16:59 +0200

    FileWriter und FileReader in UCI

--- chessengine/src/chessengine/UCI.java
+++ chessengine/src/chessengine/UCI.java
@@ -4,7 +4,15 @@
 package chessengine;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * UCI-Klasse, dient zur Kommunikation zwischen Engine und der GUI.
@@ -355,6 +363,29 @@ public class UCI implements UCI_Interface, Runnable {
         return bishopValue;
     }
 
+    private void writeValues(File datei) throws IOException{
+        FileWriter ausgabe = null;
+        try {
+            ausgabe = new FileWriter(datei);
+            ausgabe.write(queenValue + "\n" + rookValue + "\n" + knightValue +
+                            "\n" + bishopValue);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(UCI.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ausgabe.close();
+        }
+    }
+    
+    private void readValues(File datei) throws IOException{
+        FileReader eingabe = null;
+        try {
+            eingabe = new FileReader(datei);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(UCI.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            eingabe.close();
+        }
+    }
     /**
      * Threadstart fuer das Einlesen der Kommandos.
      */
